5f3442afbaf1e514228e6dcc7aa1456da2799f4f
Aidan Murray <aidanhansm@gmail.com>
2020-08-19 13:17:47 -0600

    Add and initialize School class

---
+++ lib/school.rb
@@ -0,0 +1,11 @@
+class School
+  attr_reader :hours_in_school_day, :start_time, :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @hours_in_school_day = hours_in_school_day
+    @start_time = start_time
+    @student_names = []
+  end
+
+  
+end
