b71de02acf1d41d713726440e87059ef164b6986
Aidan Murray <aidanhansm@gmail.com>
2020-08-19 13:35:34 -0600

    Complete Iteration 3

--- lib/school.rb
+++ lib/school.rb
@@ -14,4 +14,14 @@ class School
   def end_time
     "#{start_time.delete_suffix(':00').to_i + hours_in_school_day}:00"
   end
+
+  def full_time?
+    #Changed is_full_time? to full_time? to align more with Ruby conventions
+
+    hours_in_school_day > 4
+  end
+
+  def standard_student_names
+    student_names.map(&:capitalize)
+  end
 end
